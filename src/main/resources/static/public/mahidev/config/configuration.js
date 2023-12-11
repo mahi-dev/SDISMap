8da64f9054d41f68a7c502ea9c918629fe78e81c
Omar MAHI <omar.mahi@agysoft.fr>
2023-12-11 18:06:19 +0100

    ajout ssl

--- src/main/resources/static/public/mahidev/config/configuration.js
+++ src/main/resources/static/public/mahidev/config/configuration.js
@@ -1,6 +1,7 @@
+const protocol = window.location.protocol;
 const base = window.location.host;
 
 export const SETTINGS = {
-    baseApiUrl: `http://${base}`,
+    baseApiUrl: `${protocol}//${base}`,
     sdisApi: '/api/sdis'
 }
